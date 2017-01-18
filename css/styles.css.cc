9c2c7b81adcf88eac221801293e4b261fbf97da9
German Alvarez Montoya <germanalvarezmontoya@gmail.com>
2017-01-17 17:00:08 -0700

    CSS Hojas de estilos

--- css/styles.css.cc
+++ css/styles.css.cc
@@ -1,4 +1,7 @@
 /* Reglas CSS */
+/*Regla{
+    propiedad = valor
+}
 /*Tipo etiqueta*/
 body,h1,h2,h3,h4,h5,h6{
     margin: 0;
@@ -14,3 +17,37 @@ body,h1,h2,h3,h4,h5,h6{
     color: rgb(255, 255, 0,.5);
     color: white;*/
 }
+/*Tipo close*/
+#login-form{
+    width: 370px;
+    margin: 0 auto;
+    padding: 15px;
+
+    /*
+    margin: 10px; Aplica los 4 lados
+    margin: 10px 15px; Aplica arriva y abajo 10 px  izq y der 15 px
+    margin: 5px 10px 15px;   Aplica 5 arriva 10 izq y der y 15 abajo
+    margin 1px 2px 3px 
+
+    
+    */
+}
+
+#login-form .input{
+width: 100px;
+font-size: 1em;
+}
+#login-form .control
+{
+    margin-bottom: 20px;
+}
+
+#login-form .control label{
+    font-size: 1.2em;
+
+}
+
+.negrita{
+    font-weight: bold
+}
+
