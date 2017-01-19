4fb9e1b5cc7601c583a110f1a2d122a44e99d8ee
German Alvarez Montoya <germanalvarezmontoya@gmail.com>
2017-01-18 17:07:34 -0700

    CSS Y JS
    Iniciando con js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
-//TODO Agregar cdigo JavaScript
+//TODO Agregar codigo JavaScript
+//alert("Hola Mundo");
+Console.log('Hola desde la consola de javascript');
+function login(){
+    var usuario = document.getElementById('txtusuario').value;
+    var password = document-getElementById('txtpassword').value;
+    alert('Usuario: ' + usuario + ', Contraseña: ' + password);
+}
