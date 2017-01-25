4a159ace35d890569313ff090165d00c08c1cf3a
German Alvarez Montoya <germanalvarezmontoya@gmail.com>
2017-01-24 17:01:30 -0700

    Usuarios

--- conexion.php
+++ conexion.php
@@ -1,6 +1,7 @@
 <?php
 class Conexion extends mysqli{
-    private $datos = array('$database_host'=>'',
+    private $datos = array(
+                      '$database_host'=>'',
                       '$database_name'=>'',
                       '$database_user'=>'',
                       '$database_paswd'=>'',
@@ -8,34 +9,28 @@ class Conexion extends mysqli{
                      );
           
    public function __construct(){
-      $this->$datos = array (
+      $this->$datos = array(
                   '$database_host'=>'127.0.0.1',
                   '$database_name'=>'udo',
                   '$database_user'=>'root',
                   '$database_paswd'=>'toor',
                   '$database_port'=>'3306'
                  );
-                $this->conectar();
+                 parent::__construct($this->databse_host, $this->database_user, $this->database_passwd, $this->database_name, $this->database_port );
+               // $this->conectar();
    }
    
-    public function __construct1($host, $name, $user, $passwd, $port) {
+    public function __construct1($host, $user, $passwd, $name, $port) {
       $this->$datos = array(
                   '$database_host'=>$host,
-                  '$database_name'=>$name,
                   '$database_user'=>$user,
                   '$database_paswd'=>$passwd,
+                  '$database_name'=>$name,
                   '$database_port'=>$port
       );
-               $this->conectar();
-   }
-
-   public function conectar(){
-$this->connect( $this->database_host, 
-                $this->database_user,
-                $this->database_passwd,
-                $this->database_name,
-                $this->database_port);
-   }
+              // $this->conectar();
+              parent::__construct($host, $user, $passwd, $name, $port);
+   }  
 
     public function __get($keyname){
         if(array_key_exists($keyname, $this->$datos))
@@ -48,7 +43,7 @@ $this->connect( $this->database_host,
     }    
 }
 
-$objeto = new Conexion();
-$rst = $objeto->query('select * from usuario');
-$r= $rst->fetch_assoc();
-var_dump($r);
+//$objeto = new Conexion();
+//$rst = $objeto->query('select * from usuario');
+//$r= $rst->fetch_assoc();
+//var_dump($r);
