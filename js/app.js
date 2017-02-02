8f2f9c9d3c88928154f1c2ff4653ed08e345300c
German Alvarez Montoya <germanalvarezmontoya@gmail.com>
2017-02-01 17:07:15 -0700

    Validacion JS
    perfil de usuario con variables de session

--- js/app.js
+++ js/app.js
@@ -3,15 +3,50 @@
 //Console.log('Hola desde la consola de javascript');
 
 function login(usuario, passwd){
- $.post("login.php",{ txtusuario:usuario, txtpassword:passwd }, function(data){
-     return data;
+ $.post("login.php",{ txtusuario:usuario, txtpassword:passwd }, function(response){
+     var acceso = response.data.login;
+     if ( acceso == true){
+         var usuario = response.data.usuario;
+         sessionStorage.userid = usuario.id;
+         sessionStorage.username = usuario.username;
+         sessionStorage.nombre = usuario.nombre;
+         sessionStorage.apellidos = usuario.apellidos;
+         sessionStorage.email = usuario.email;
+         console.log(usuario);
+     } else if(acceso == false){
+         $('#login-form-msg').html('Usuario o contraseña incorrecta');
+         $('#login-form-msg').removeClass('oculto');
+         $('#txtpassword').val('');
+          console.warn('acceso incorrecto');
+     } else{
+          $('#login-form-msg').html('Error en la conexion a la base de datos');
+          $('#login-form-msg').removeClass('oculto');
+         console.error('error en la consulta');
+     }    
  });
 }
+
 $(document).ready(function() {
     $('#login-button').click(function(event){
      event.preventDefault(); 
      var usuario = $('#txtusuario').val();
      var passwd = $('#txtpassword').val();  
-     login(usuario, passwd) 
-    });
+     if (usuario == ''){
+         //quitra la clase oculto del dic de error de txtusuario
+         $('#txtusuario+div').removeClass('oculto');
+
+     } else {
+         $('#txtusuario+div').addClass('oculto');
+     }
+     if(passwd == ''){
+         // quitar la clase oculto del div de error de txtpassword
+          $('#txtpassword+div').removeClass('oculto');
+     }else {
+          $('#txtpassword+div').addClass('oculto');
+     }
+    
+     if(usuario != '' && passwd != ''){
+         login(usuario, passwd);
+     } 
+  });
 });
